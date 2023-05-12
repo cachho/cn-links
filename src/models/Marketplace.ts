166f240681499d57ba957c517db06845c8c418bb
cachho <admin@ch-webdev.com>
2023-05-13 00:50:40 +0200

    feat: added marketplace with tld

--- src/models/Marketplace.ts
+++ src/models/Marketplace.ts
@@ -1,1 +1,10 @@
-export type Marketplace = 'weidian' | 'taobao' | '1688' | 'tmall';
+export const marketplaces = ['weidian', 'taobao', '1688', 'tmall'] as const;
+export const marketplacesWithTld = [
+  'weidian.com',
+  'taobao.com',
+  '1688.com',
+  'tmall.com',
+] as const;
+
+export type Marketplace = (typeof marketplaces)[number];
+export type MarketplaceWithTld = (typeof marketplacesWithTld)[number];

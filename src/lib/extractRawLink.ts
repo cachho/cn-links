2264cb0528b3a5c5719bd2b503b86d95284f15b7
cachho <admin@ch-webdev.com>
2023-05-13 01:32:21 +0200

    feat: handle cssbuy

--- src/lib/extractRawLink.ts
+++ src/lib/extractRawLink.ts
@@ -1,5 +1,16 @@
-export function extractRawLink(href: string | URL): URL | undefined {
+import { decryptCssbuy } from './decryptCssbuy';
+import { detectAgent } from './detectAgent';
+
+export function extractRawLink(
+  href: string | URL,
+  cantBeCssbuy?: boolean
+): URL | undefined {
   const link = href instanceof URL ? href : new URL(href);
+
+  if (!cantBeCssbuy && detectAgent(link.href) === 'cssbuy') {
+    return decryptCssbuy(link);
+  }
+
   const urlParams = new URLSearchParams(link.search ?? link);
   const innerParam = urlParams.get('url');
 

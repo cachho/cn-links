f98130ed55813cde31b8f0c3d2a2092a5087607b
cachho <admin@ch-webdev.com>
2023-05-13 19:24:35 +0200

    test: added test for agent links

--- src/lib/__tests__/extractId.test.ts
+++ src/lib/__tests__/extractId.test.ts
@@ -1,45 +1,52 @@
 import { extractId } from '../extractId';
 
 describe('extractId', () => {
-  it('should extract the correct ID for Weidian link', () => {
+  test('should extract the correct ID for Weidian link', () => {
     const href = 'https://weidian.com/item.html?itemID=3053526244';
     const id = extractId(href, 'weidian');
     expect(id).toBe('3053526244');
   });
 
-  it('should extract the correct ID for regular Taobao link', () => {
+  test('should extract the correct ID for regular Taobao link', () => {
     const href = 'https://item.taobao.com/item.html?id=674029285425';
     const id = extractId(href, 'taobao');
     expect(id).toBe('674029285425');
   });
 
-  it('should extract the correct ID for Taobao link with world subdomain', () => {
+  test('should extract the correct ID for Taobao link with world subdomain', () => {
     const href = 'https://world.taobao.com/item/674029285425.htm';
     const id = extractId(href, 'taobao');
     expect(id).toBe('674029285425');
   });
 
-  it('should extract the correct ID for 1688 link', () => {
+  test('should extract the correct ID for 1688 link', () => {
     const href = 'https://detail.1688.com/offer/610494659403.html';
     const id = extractId(href, '1688');
     expect(id).toBe('610494659403');
   });
 
-  it('should extract the correct ID for Tmall link', () => {
+  test('should extract the correct ID for Tmall link', () => {
     const href = 'https://detail.tmall.com/item_o.htm?id=674029285425';
     const id = extractId(href, 'tmall');
     expect(id).toBe('674029285425');
   });
 
-  it('should return null for unknown marketplace', () => {
+  test('should return null for unknown marketplace', () => {
     const href = 'https://example.com';
     const id = extractId(href);
     expect(id).toBeUndefined();
   });
 
-  it('should successfully detect the marketplace and extract the ID', () => {
+  test('should successfully detect the marketplace and extract the ID', () => {
     const href = 'https://item.taobao.com/item.html?id=987654321';
     const id = extractId(href);
     expect(id).toBe('987654321');
+  });
+
+  test('agent link inputs should not work', () => {
+    const href =
+      'https://www.wegobuy.com/en/page/buy?from=search-input&url=https%3A%2F%2Fitem.taobao.com%2Fitem.htm%3Fid%3D674029285425&partnercode=6t86Xk';
+    const id = extractId(href, 'taobao');
+    expect(id).toBeUndefined();
   });
 });

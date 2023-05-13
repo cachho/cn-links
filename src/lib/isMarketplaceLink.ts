24fdaccb9a1d42524c38cf94c6cf627e815c9db1
cachho <admin@ch-webdev.com>
2023-05-13 18:32:39 +0200

    refactor: renamed to isMarketplaceLink

--- src/lib/isMarketplaceLink.ts
+++ src/lib/isMarketplaceLink.ts
@@ -7,7 +7,7 @@ import { getDomainFromHostname } from './getDomainFromHostname';
  * @param {string | URL} href - The URL or hostname to check.
  * @returns {boolean} True if the link corresponds to a marketplace, false otherwise.
  */
-export function isMarketplace(href: string | URL): boolean {
+export function isMarketplaceLink(href: string | URL): boolean {
   const hostname =
     typeof href === 'string' ? new URL(href).hostname : href.hostname;
   const domain = getDomainFromHostname(hostname);

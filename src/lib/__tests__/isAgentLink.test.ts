354137dc35ae33ecc3b90c32ab6656b4fe5451f4
cachho <admin@ch-webdev.com>
2023-05-13 18:44:01 +0200

    test: added weidian test

--- src/lib/__tests__/isAgentLink.test.ts
+++ src/lib/__tests__/isAgentLink.test.ts
@@ -17,6 +17,7 @@ describe('isAgentLink', () => {
       'https://detail.tmall.com/item_o.htm?id=674029285425&pcdegrade=true&de_count=1',
       'https://item.taobao.com/item.htm?id=674029285425',
       'https://detail.1688.com/offer/610494659403.html',
+      'https://weidian.com/item.html?itemID=5789470155',
     ];
 
     nonAgentLinks.forEach((link) => {

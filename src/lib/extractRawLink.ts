a6b911ab8bb268ba97e7bfc112b03e90c0cc69f3
cachho <admin@ch-webdev.com>
2023-05-13 19:17:38 +0200

    docs: internal declaration

--- src/lib/extractRawLink.ts
+++ src/lib/extractRawLink.ts
@@ -2,10 +2,12 @@ import { decryptCssbuy } from './decryptCssbuy';
 import { detectAgent } from './detectAgent';
 
 /**
+ * @Internal
  * Extracts the raw link from the search params of a provided (agent) URL.
+ * Is internal because end-users can use the `toRaw` or `generateRawLinks` to extract a raw link with more flexible inputs.
  *
  * @param {string | URL} href - The URL from which to extract the raw link.
- * @param {boolean} [cantBeCssbuy] - Indicates whether the raw link cannot be from the 'cssbuy' agent. If this is true the call to detectAgent is skipped. Default is false.
+ * @param {boolean} [cantBeCssbuy] - Indicates whether the raw link cannot be from the 'cssbuy' agent. If this is true the call to detectAgent is skipped. *Legacy functionality that makes little sense on a internal function.* Default is false.
  * @returns {URL | undefined} The extracted raw link as a URL object, or undefined if no raw link is found.
  */
 export function extractRawLink(

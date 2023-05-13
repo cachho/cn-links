6ca1fb76632c055123c8bf79a8d29c83ce34cc53
cachho <admin@ch-webdev.com>
2023-05-13 19:06:00 +0200

    feat: added index

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { detectAgent } from './lib/detectAgent';
+import { detectMarketplace } from './lib/detectMarketplace';
+import { extractId } from './lib/extractId';
+import { generateAgentLink } from './lib/generateAgentLink';
+import { generateProperLink } from './lib/generateProperLink';
+import { isAgentLink } from './lib/isAgentLink';
+import { isMarketplaceLink } from './lib/isMarketplaceLink';
+import { toAgent } from './lib/toAgent';
+import { toRaw } from './lib/toRaw';
+
+export {
+  detectAgent,
+  detectMarketplace,
+  extractId,
+  generateAgentLink,
+  generateProperLink,
+  isAgentLink,
+  isMarketplaceLink,
+  toAgent,
+  toRaw,
+};

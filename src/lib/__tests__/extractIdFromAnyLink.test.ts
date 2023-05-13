2a4ad8be0bb447cde0197b9ff4a6ab1f2a5a42cf
cachho <admin@ch-webdev.com>
2023-05-13 19:49:38 +0200

    feat: extract id from any link

---
+++ src/lib/__tests__/extractIdFromAnyLink.test.ts
@@ -0,0 +1,52 @@
+import { extractIdFromAnyLink } from '../extractIdFromAnyLink';
+
+describe('extractId', () => {
+  test('should extract the correct ID for Weidian link', () => {
+    const href = 'https://weidian.com/item.html?itemID=3053526244';
+    const id = extractIdFromAnyLink(href, 'weidian');
+    expect(id).toBe('3053526244');
+  });
+
+  test('should extract the correct ID for regular Taobao link', () => {
+    const href = 'https://item.taobao.com/item.html?id=674029285425';
+    const id = extractIdFromAnyLink(href, 'taobao');
+    expect(id).toBe('674029285425');
+  });
+
+  test('should extract the correct ID for Taobao link with world subdomain', () => {
+    const href = 'https://world.taobao.com/item/674029285425.htm';
+    const id = extractIdFromAnyLink(href, 'taobao');
+    expect(id).toBe('674029285425');
+  });
+
+  test('should extract the correct ID for 1688 link', () => {
+    const href = 'https://detail.1688.com/offer/610494659403.html';
+    const id = extractIdFromAnyLink(href, '1688');
+    expect(id).toBe('610494659403');
+  });
+
+  test('should extract the correct ID for Tmall link', () => {
+    const href = 'https://detail.tmall.com/item_o.htm?id=674029285425';
+    const id = extractIdFromAnyLink(href, 'tmall');
+    expect(id).toBe('674029285425');
+  });
+
+  test('should return null for unknown marketplace', () => {
+    const href = 'https://example.com';
+    const id = extractIdFromAnyLink(href);
+    expect(id).toBeUndefined();
+  });
+
+  test('should successfully detect the marketplace and extract the ID', () => {
+    const href = 'https://item.taobao.com/item.html?id=987654321';
+    const id = extractIdFromAnyLink(href);
+    expect(id).toBe('987654321');
+  });
+
+  test('agent link inputs should also work', () => {
+    const href =
+      'https://www.wegobuy.com/en/page/buy?from=search-input&url=https%3A%2F%2Fitem.taobao.com%2Fitem.htm%3Fid%3D674029285425&partnercode=6t86Xk';
+    const id = extractIdFromAnyLink(href, 'taobao');
+    expect(id).toBe('674029285425');
+  });
+});
---
+++ src/lib/extractIdFromAnyLink.ts
@@ -0,0 +1,28 @@
+import type { Marketplace } from '../models';
+import { extractId } from './extractId';
+import { isRawLink } from './isRawLink';
+import { toRaw } from './toRaw';
+
+/**
+ * Slower implementation of `extractId` that works with raw links and agent links.
+ *
+ * @param {string | URL} href - The URL from which to extract the ID.
+ * @param {Marketplace} [marketplace] - The marketplace to consider for ID extraction. If not provided, it will be automatically detected.
+ * @returns {string | undefined} The extracted ID, or undefined if no ID is found.
+ */
+export function extractIdFromAnyLink(
+  href: string | URL,
+  marketplace?: Marketplace
+): string | undefined {
+  const link = href instanceof URL ? href : new URL(href);
+
+  if (isRawLink(link)) {
+    return extractId(link, marketplace);
+  }
+
+  const rawLink = toRaw(link);
+  if (rawLink) {
+    return extractId(rawLink, marketplace);
+  }
+  return undefined;
+}

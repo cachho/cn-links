7a837c8269cd2b62ec20c4879b29413e96185d48
cachho <admin@ch-webdev.com>
2023-05-14 10:33:26 +0200

    chore: added arrays to index

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,16 +1,32 @@
-import { Agent, AgentWithRaw } from './Agent';
-import {
+import type { Agent, AgentWithRaw } from './Agent';
+import { agents } from './Agent';
+import type {
   Marketplace,
   MarketplaceWithTld,
   NonLinkMarketplace,
   NonLinkMarketplaceWithTld,
 } from './Marketplace';
+import {
+  marketplaces,
+  marketplacesWithNonLinkMarketplaces,
+  marketplacesWithNonLinkMarketplacesWithTld,
+  marketplacesWithTld,
+  nonLinkMarketplaces,
+  nonLinkMarketplacesWithTld,
+} from './Marketplace';
 
 export {
   Agent,
+  agents,
   AgentWithRaw,
   Marketplace,
+  marketplaces,
+  marketplacesWithNonLinkMarketplaces,
+  marketplacesWithNonLinkMarketplacesWithTld,
+  marketplacesWithTld,
   MarketplaceWithTld,
   NonLinkMarketplace,
+  nonLinkMarketplaces,
+  nonLinkMarketplacesWithTld,
   NonLinkMarketplaceWithTld,
 };

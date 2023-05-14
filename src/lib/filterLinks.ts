d2806c635858d1f9c738be00282872f425f7cacb
cachho <admin@ch-webdev.com>
2023-05-14 10:33:59 +0200

    feat: added non link marketplaces to filter

--- src/lib/filterLinks.ts
+++ src/lib/filterLinks.ts
@@ -1,4 +1,5 @@
 import { isAgentLink } from './isAgentLink';
+import { isNonLinkMarketplace } from './isNonLinkMarketplace';
 import { isRawLink } from './isRawLink';
 
 /**
@@ -7,6 +8,7 @@ import { isRawLink } from './isRawLink';
  * @param {string} text - The string to filter for links.
  * @param {boolean} agentLinks - Returns agent links if true. Default true.
  * @param {boolean} rawLinks - Returns raw links if true. Default true.
+ * @param {boolean} nonMarketplaceLinks - Returns non-link marketplace links if true. Default false.
  * @param {number} [limit] - The maximum number of results to return.
  * @returns {string[]} An array of links that evaluate to true for either `isAgentLink` or `isRawLink`.
  */
@@ -14,9 +16,10 @@ export function filterLinks(
   text: string,
   agentLinks: boolean = true,
   rawLinks: boolean = true,
+  nonMarketplaceLinks: boolean = false,
   limit?: number
 ): string[] {
-  if (!agentLinks && !rawLinks) {
+  if (!agentLinks && !rawLinks && !nonMarketplaceLinks) {
     return [];
   }
 
@@ -26,7 +29,9 @@ export function filterLinks(
   const filteredLinks = links
     .filter(
       (link) =>
-        (agentLinks && isAgentLink(link)) || (rawLinks && isRawLink(link))
+        (agentLinks && isAgentLink(link)) ||
+        (rawLinks && isRawLink(link)) ||
+        (nonMarketplaceLinks && isNonLinkMarketplace(link))
     )
     .slice(0, limit);
 

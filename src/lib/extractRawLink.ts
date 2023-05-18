6679c2091d9ed96aa7592151c9a9d5318e1245a7
cachho <admin@ch-webdev.com>
2023-05-18 14:27:57 +0200

    fix: assume typeguarded

--- src/lib/extractRawLink.ts
+++ src/lib/extractRawLink.ts
@@ -1,4 +1,4 @@
-import { AgentLink, RawLink, RawURL } from '../models/LinkTypes';
+import { AgentLink, AgentURL, RawLink, RawURL } from '../models/LinkTypes';
 import { decryptCssbuy } from './decryptCssbuy';
 import { detectAgent } from './detectAgent';
 
@@ -7,26 +7,22 @@ import { detectAgent } from './detectAgent';
  * Extracts the raw link from the search params of a provided (agent) URL.
  * Is internal because end-users can use the `toRaw` or `generateRawLinks` to extract a raw link with more flexible inputs.
  *
- * @param {AgentLink} href - The URL from which to extract the raw link.
+ * @param {AgentURL} href - The URL from which to extract the raw link. It's assumed that it's typeguarded.
  * @param {boolean} [cantBeCssbuy] - Indicates whether the raw link cannot be from the 'cssbuy' agent. If this is true the call to detectAgent is skipped. *Legacy functionality that makes little sense on a internal function.* Default is false.
- * @returns {RawURL | undefined} The extracted raw link as a URL object, or undefined if no raw link is found.
+ * @returns {RawURL} The extracted raw link as a URL object, or undefined if no raw link is found.
  */
 export function extractRawLink(
-  href: AgentLink,
+  href: AgentURL,
   cantBeCssbuy?: boolean
-): RawURL | undefined {
+): RawURL {
   const link = href instanceof URL ? href : new URL(href);
 
   if (!cantBeCssbuy && detectAgent(link.href) === 'cssbuy') {
-    return decryptCssbuy(link);
+    return decryptCssbuy(link)!; // Forced because it's assumed that agentUrl is valid.
   }
 
   const urlParams = new URLSearchParams(link.search ?? link);
   const innerParam = urlParams.get('url');
 
-  if (!innerParam) {
-    return undefined;
-  }
-
-  return new URL(innerParam);
+  return new URL(innerParam!); // Forced because it's assumed that agentUrl is valid.
 }

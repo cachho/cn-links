02d4274caa19d0c25af2d1d285195d25e5f63cc9
cachho <admin@ch-webdev.com>
2023-05-18 15:26:32 +0200

    feat: infer strong typing

--- src/lib/extractIdFromAnyLink.ts
+++ src/lib/extractIdFromAnyLink.ts
@@ -1,4 +1,4 @@
-import type { Id, Marketplace } from '../models';
+import type { AgentURL, Id, Marketplace, RawURL } from '../models';
 import { extractId } from './extractId';
 import { isRawLink } from './isRawLink';
 import { toRaw } from './toRaw';
@@ -6,23 +6,23 @@ import { toRaw } from './toRaw';
 /**
  * Slower implementation of `extractId` that works with raw links and agent links.
  *
- * @param {string | URL} href - The URL from which to extract the ID.
+ * @param {AgentURL | RawURL} href - The URL from which to extract the ID.
  * @param {Marketplace} [marketplace] - The marketplace to consider for ID extraction. If not provided, it will be automatically detected.
- * @returns {Id | undefined} The extracted ID, or undefined if no ID is found.
+ * @returns {Id} The extracted ID, or undefined if no ID is found.
  */
 export function extractIdFromAnyLink(
   href: string | URL,
   marketplace?: Marketplace
-): Id | undefined {
+): Id {
   const link = href instanceof URL ? href : new URL(href);
 
   if (isRawLink(link)) {
     return extractId(link, marketplace);
+  } else {
+    const rawLink = toRaw(link);
+    if (rawLink) {
+      return extractId(rawLink, marketplace);
+    }
+    throw new Error(`Id could not be extracted from string: ${link.href}`);
   }
-
-  const rawLink = toRaw(link);
-  if (rawLink) {
-    return extractId(rawLink, marketplace);
-  }
-  return undefined;
 }

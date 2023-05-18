a254b4a8825c09f448982b231040662e09ff2c97
cachho <admin@ch-webdev.com>
2023-05-18 13:18:01 +0200

    feat: URL typing

--- src/models/LinkTypes.ts
+++ src/models/LinkTypes.ts
@@ -4,18 +4,19 @@
  * Comprised of the agent domain and an inner Marketplace link 
  * with a marketplace domain an id.
  */
-export type AgentLink = string;
+export type AgentLink = URL;
 
 /**
- * **Link to a marketplace.** 
+ * **Link to a marketplace. This could be Taobao, Weidian, 1688 or Tmall.** 
  */
-export type MarketplaceLink = string;
+export type MarketplaceLink = URL;
 
 /**
  * **Link to a non-link marketplace.**
+ * This could be Yupoo.
  * These marketplaces are part of this library for feature completeness,
  * (mainly the filter).
  * Since these links are composed differently,
  * most transformations are impossible.
  */
-export type NonLinkMarketplaceLink = string;
+export type NonLinkMarketplaceLink = URL;

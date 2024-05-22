ac9f165fbd10b4d219905d12d2abfee49fd1e96e
cachho <admin@ch-webdev.com>
2024-05-22 23:01:20 +0200

    feat: add is agent link

---
+++ src/store/__tests__/isAgentLink.test.ts
@@ -0,0 +1,58 @@
+import { isAgentLink } from '../isAgentLink';
+
+describe('isAgentLink', () => {
+  it('should return true for pandabuy store link', () => {
+    const href =
+      'https://www.pandabuy.com/shopdetail?ra=21&t=wd&id=1625671124&o=weidian.com';
+    expect(isAgentLink(href)).toBe(true);
+  });
+
+  it('should return true for cnfans store link', () => {
+    const href =
+      'https://cnfans.com/shops/?shop_type=taobao&num=1&sort=default&shop_id=277184856';
+    expect(isAgentLink(href)).toBe(true);
+  });
+
+  test('returns false for non-agent links', () => {
+    const nonAgentLinks = [
+      'https://m.1688.com/winport/b2b-22108725372987d9fa.html',
+      'https://shop232138271.v.weidian.com/?userid=232138271',
+      'https://weidian.com/?userid=1625671124',
+    ];
+
+    nonAgentLinks.forEach((link) => {
+      expect(isAgentLink(link)).toBe(false);
+    });
+  });
+
+  test('returns false for non-valid URL input', () => {
+    const invalidLinks = ['invalid-url', 'https://', 'ftp://example.com'];
+
+    invalidLinks.forEach((link) => {
+      expect(isAgentLink(link)).toBe(false);
+    });
+  });
+
+  test('returns false for inputs to domain root without item path', () => {
+    const rootLinks = [
+      'https://www.wegobuy.com/',
+      'https://www.pandabuy.com/',
+      'https://detail.tmall.com/',
+      'https://item.taobao.com/',
+      'https://detail.1688.com/',
+      'https://weidian.com/',
+    ];
+
+    rootLinks.forEach((link) => {
+      expect(isAgentLink(link)).toBe(false);
+    });
+  });
+
+  it('should return false if the link is not a valid cn-link', () => {
+    it('should return true for pandabuy store link', () => {
+      const href =
+        'https://www.pandabuy.com/NOTshopdetail?ra=21&t=wd&id=1625671124&o=weidian.com';
+      expect(isAgentLink(href)).toBe(true);
+    });
+  });
+});
---
+++ src/store/isAgentLink.ts
@@ -0,0 +1,36 @@
+import { extractRawLink } from '../item/extractRawLink';
+import { detectAgent } from '../lib/detectAgent';
+import { isRawLink } from './isRawLink';
+
+/**
+ * Checks if the provided URL or hostname corresponds to an agent link.
+ * It's important that it is an item link, agent links to other pages on their website return false,
+ * for this scenario you should just use the hostname, but you can also set simpleDomainCheck to true.
+ *
+ * @param {string | URL} href - The URL or hostname to check.
+ * @returns {boolean} True if the link corresponds to an agent, false otherwise.
+ */
+export function isAgentLink(href: string | URL): boolean {
+  let link: URL;
+  try {
+    link = new URL(href);
+  } catch {
+    return false;
+  }
+
+  // Check if the URL protocol is valid
+  if (!['http:', 'https:'].includes(link.protocol)) {
+    return false;
+  }
+
+  const agent = detectAgent(link);
+
+  if (!agent) return false;
+
+  try {
+    const rawLink = extractRawLink(link);
+    return isRawLink(rawLink);
+  } catch {
+    return false;
+  }
+}

b5ea7fa9b2040189fe1d05f4ebfd8c3ec225f5b3
cachho <admin@ch-webdev.com>
2024-06-30 21:58:50 +0200

    feat: hubbuycn legacy links

--- src/item/__tests__/extractRawLink.test.ts
+++ src/item/__tests__/extractRawLink.test.ts
@@ -231,6 +231,15 @@ describe('extractRawLink', () => {
     );
   });
 
+  it('should work for hubbuy direct links', () => {
+    const href =
+      'https://www.hubbuycn.com/taobaoID=726526607194&inviter=johnerik123';
+    const rawLink = extractRawLink(new URL(href));
+    expect(rawLink).toEqual(
+      new URL('https://item.taobao.com/item.htm?id=726526607194')
+    );
+  });
+
   it('should throw an error for kameymall purchase history links', () => {
     const href =
       'https://www.kameymall.com/purchases/1730295605736697858/%E3%80%90%E6%9C%80%E9%AB%98%E7%89%88%E6%9C%AC%E3%80%91%E7%99%BE%E6%90%ADchrome-hearts%E5%85%8B%E7%BD%97%E5%BF%83%E9%A1%B9%E9%93%BE-%E5%85%8B%E7%BD%97%E5%BF%83%E5%8F%8C%E5%8D%81%E5%AD%97%E6%9E%B6%E6%83%85%E4%BE%A3%E9%A1%B9%E9%93%BE-%E6%BD%AE%E6%B5%81%E7%94%B7%E5%A3%AB%E5%A5%B3%E5%A3%AB%E5%8D%81%E5%AD%97%E6%9E%B6%E9%A1%B9%E9%93%BE%E5%A5%97%E9%93%BE';

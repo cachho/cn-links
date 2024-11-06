5f5b3b0d8a66031ed20d33e46a45554cabaca74c
cachho <admin@ch-webdev.com>
2024-11-06 11:56:09 +0100

    fix: invite code

--- src/item/__tests__/isAgentLink.test.ts
+++ src/item/__tests__/isAgentLink.test.ts
@@ -15,7 +15,7 @@ describe('isAgentLink', () => {
       'https://joyabuy.com/es/product/?shop_type=taobao&id=726526607194',
       'https://www.lovegobuy.com/pc/#/goods/detail?platform=weidian&goodsId=4480454092',
       'https://www.lovegobuy.com/product?platform=weidian&goodsId=4480454092',
-      'https://m.lovegobuy.com/product?id=7247608954&shop_type=weidian&invite_code=JADESHIP',
+      'https://m.lovegobuy.com/product?id=7247608954&shop_type=weidian&invite_code=123456',
     ];
 
     agentLinks.forEach((link) => {
